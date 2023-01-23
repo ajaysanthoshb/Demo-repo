358183da99c306fff8c614d7b060888036c06225
aboyina <ajaysanthoshb@gmail.com>
2023-01-23 13:31:39 +0530

    change print

--- hungry.py
+++ hungry.py
@@ -4,7 +4,5 @@ if n == "HUNGRY":
     print("I like samosa")
     print("I like burger")
 else:
-    thristy = input("are u thirsty")
-    if thirsty:
-        print("drink water")
+    print("namasthe")
     
