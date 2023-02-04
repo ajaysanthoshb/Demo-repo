2c0fb82c47f89a5c0e46ad05a51c8588a6e60348
Kamesh Kumar Sivakumar <kameshskumar.17@gmail.com>
2023-02-04 12:09:34 +0530

    Modified hungry.py
    Added on elif to stop the program

--- hungry.py
+++ hungry.py
@@ -3,6 +3,9 @@ if n == "HUNGRY":
     print("print HUNGRY")
     print("I like samosa")
     print("I like burger")
+elif n == "--stop":
+    print("print STOP")
+    exit(0)
 else:
     thristy = input("are u thirsty")
     print("git pull")
